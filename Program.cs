3dde5a4297ed61a5a9660473c3a4b2e364c74b4f
anmesk <an_mesk@live.com>
2020-01-15 14:39:16 -0500

    Fortune teller, quit feature *still* in progress

--- Program.cs
+++ Program.cs
@@ -6,100 +6,139 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
-            /*do {
-
-            }*/
-            
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            int age = int.Parse(Console.ReadLine());
+            // string firstName, lastName, age, month, color, years, balance = "";
+            string firstName = "";
+            string lastName = "";
+            string age = "";
+            int ageValue = 0;
+            string month = "";
+            int monthValue = 0;
+            string color = "";
             string years = "";
-            Console.WriteLine("What is your month of birth (as a number)?");
-            int month = int.Parse(Console.ReadLine());
+            string siblings = "";
+            int siblingsValue = 0;
+
             string balance = "";
-            bool colorCheck = false;
-            string color;
+
+            bool spellEffect = true;
             do {
-                Console.WriteLine("What is your favorite ROYGBIV color? (Enter 'Help' for a list of ROYGBIV colors.)");
-                color = Console.ReadLine();
-                if (color == "Help") { // implement case sensitivity possibly maybe???
-                    Console.WriteLine("R = red, O = orange, Y = yellow, G = green, B = blue, I = indigo, V = violet.");
+                if (firstName == "Quit" || lastName == "Quit" || age == "Quit" || month == "Quit" || color == "Quit") {
+                    Console.WriteLine("Nobody likes a quitter...");
+                    return;
                 }
-                else colorCheck = true;
-            } while (colorCheck == false);
-            
-            // char color = Console.ReadKey().KeyChar;
-            string vehicle = "";
-            Console.WriteLine("How many siblings do you have?");
-            int siblings = int.Parse(Console.ReadLine());
-            string location = "";
+                Console.WriteLine("What is your first name?");
+                firstName = Console.ReadLine();
+                Console.WriteLine("What is your last name?");
+                lastName = Console.ReadLine();
+                Console.WriteLine("How old are you?");
+                age = Console.ReadLine();
+                ageValue = Convert.ToInt32(age);
+                Console.WriteLine("What is your month of birth (as a number)?");
+                month = Console.ReadLine();
+                monthValue = Convert.ToInt32(month);
+                bool colorCheck = false;
+                do
+                {
+                    Console.WriteLine("What is your favorite ROYGBIV color? (Enter 'Help' for a list of ROYGBIV colors.)");
+                    color = Console.ReadLine();
+                    if (color == "Help")
+                    { // implement case sensitivity possibly maybe???
+                        Console.WriteLine("R = red, O = orange, Y = yellow, G = green, B = blue, I = indigo, V = violet.");
+                    }
+                    else colorCheck = true;
+                } while (colorCheck == false);
 
-            // Years until retirement
-            if (age % 2 == 0) {
-                years = "12";
-            }
-            else years = "14";
+                // char color = Console.ReadKey().KeyChar;
+                string vehicle = "";
+                Console.WriteLine("How many siblings do you have?");
+                siblings = Console.ReadLine();
+                siblingsValue = Convert.ToInt32(siblings);
 
-            // Vacation home location
-            if (siblings == 0) {
-                location = "Boca Raton, FL";
-            }
-            else if (siblings == 1) {
-                location = "Nassau, Bahamas";
-            }
-            else if (siblings == 2) {
-                location = "Ponte Negra, Brazil";
-            }
-            else if (siblings == 3) {
-                location = "Portland, OR";
-            }
-            else if (siblings > 3) {
-                location = "Baton Rouge, LA";
-            }
-            else if (siblings < 0) {
-                location = "Chernobyl, Ukraine";
-            }
+                string location = "";
 
-            // Mode of transportation
-            if (color == "red") {
-                vehicle = "Maserati";
-            }
-            if (color == "orange") {
-                vehicle = "stallion";
-            }
-            if (color == "yellow") {
-                vehicle = "chariot";
-            }
-            if (color == "green") {
-                vehicle = "taxi";
-            }
-            if (color == "blue") {
-                vehicle = "rickshaw";
-            }
-            if (color == "indigo") {
-                vehicle = "motor scooter";
-            }
-            if (color == "red") {
-                vehicle = "flying saucer";
-            }
-
-            // Bank balance
-            if (month >= 1 && month <= 12) {
-                if (month >= 1 && month <= 4) {
-                    balance = "$256,000.76";
+                // Years until retirement
+                if (ageValue % 2 == 0)
+                {
+                    years = "12";
                 }
-                if (month >= 5 && month <= 8) {
-                    balance = "$3,687,105.42";
+                else years = "14";
+
+                // Vacation home location
+                if (siblingsValue == 0)
+                {
+                    location = "Boca Raton, FL";
                 }
-                if (month >= 9 && month <= 12) {
-                    balance = "$86.23";
+                else if (siblingsValue == 1)
+                {
+                    location = "Nassau, Bahamas";
                 }
-            }
-            else balance = "$0";
-            Console.WriteLine($"{firstName} {lastName} will retire in {years} years with {balance} balance in the bank, a vacation home in {location}, and travel by {vehicle}.");
+                else if (siblingsValue == 2)
+                {
+                    location = "Ponte Negra, Brazil";
+                }
+                else if (siblingsValue == 3)
+                {
+                    location = "Portland, OR";
+                }
+                else if (siblingsValue > 3)
+                {
+                    location = "Baton Rouge, LA";
+                }
+                else if (siblingsValue < 0)
+                {
+                    location = "Chernobyl, Ukraine";
+                }
+
+                // Mode of transportation
+                if (color == "red")
+                {
+                    vehicle = "Maserati";
+                }
+                if (color == "orange")
+                {
+                    vehicle = "stallion";
+                }
+                if (color == "yellow")
+                {
+                    vehicle = "chariot";
+                }
+                if (color == "green")
+                {
+                    vehicle = "taxi";
+                }
+                if (color == "blue")
+                {
+                    vehicle = "rickshaw";
+                }
+                if (color == "indigo")
+                {
+                    vehicle = "motor scooter";
+                }
+                if (color == "red")
+                {
+                    vehicle = "flying saucer";
+                }
+
+                // Bank balance
+                if (monthValue >= 1 && monthValue <= 12)
+                {
+                    if (monthValue >= 1 && monthValue <= 4)
+                    {
+                        balance = "$256,000.76";
+                    }
+                    if (monthValue >= 5 && monthValue <= 8)
+                    {
+                        balance = "$3,687,105.42";
+                    }
+                    if (monthValue >= 9 && monthValue <= 12)
+                    {
+                        balance = "$86.23";
+                    }
+                }
+                else balance = "$0";
+                Console.WriteLine($"{firstName} {lastName} will retire in {years} years with {balance} balance in the bank, a vacation home in {location}, and travel by {vehicle}.");
+                spellEffect = false;
+            } while (spellEffect == true);
         }
     }
 }
